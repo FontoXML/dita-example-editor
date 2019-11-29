4a9a63a26965d47c3c08e66ff8e3a67ecd97d1a9
Lorenzo de Jong <lorenzo.de.jong@liones.nl>
2019-11-29 11:57:52 +0100

    Replace Symbols masthead button with a drop button which opens a SymbolsGrid.

---
+++ packages/dita-example-special-characters/src/QuickAccessSymbolsDropButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Block, ButtonWithDrop, Drop } from 'fds/components';
+
+import FxOperationButton from 'fontoxml-fx/src/FxOperationButton.jsx';
+import SymbolsGrid from 'fontoxml-special-characters/src/SymbolsGrid.jsx';
+
+function renderDrop({ closeDrop }) {
+	return (
+		<Drop>
+			<SymbolsGrid
+				characterSet="quick-access-currency-symbols"
+				columns={6}
+				onItemClick={closeDrop}
+			/>
+
+			<Block paddingSize="s">
+				<FxOperationButton
+					label="All symbols"
+					operationName="default-special-character-insert"
+				/>
+			</Block>
+		</Drop>
+	);
+}
+
+export default function QuickAccessSymbolsDropButton() {
+	return <ButtonWithDrop icon="keyboard-o" label="Symbol" renderDrop={renderDrop} />;
+}
