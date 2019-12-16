3d4b710b25220724e736f9a7fce9474ae7ba09f5
Thomas Brekelmans <thomas.brekelmans@liones.nl>
2019-12-16 13:45:48 +0100

    DEV-5181 Integrate a working example of overriding the default context menu for cals tables with the same exact menu but now created with the new nested/grouped contextual operations API possibilities. Also add a config setting accessible via the scope to quickly switch between the old default context menu and this new menu. (Visually/functionally they are identical.)

---
+++ packages/dita-example-sx-modules-xsd-tbl-decl-mod-override/src/setDefaultConfiguration.js
@@ -0,0 +1,5 @@
+import configurationManager from 'fontoxml-configuration/src/configurationManager.js';
+
+export default function setDefaultConfiguration() {
+	configurationManager.setDefault('app/use-default-table-context-menu', false);
+}
