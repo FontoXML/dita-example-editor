f349187ce53f650359b30483920f36a4ff704872
Rober <rober.prieto@fontoxml.com>
2020-04-09 12:08:32 +0200

    Fix structureViewItem import

--- packages/dita-example-structure-view/src/configureSxModule.js
+++ packages/dita-example-structure-view/src/configureSxModule.js
@@ -1,5 +1,5 @@
 import configureContextualOperations from 'fontoxml-families/src/configureContextualOperations.js';
-import configureAsStructureViewItem from 'fontoxml-families/src/configureasstructureviewitem.js';
+import configureAsStructureViewItem from 'fontoxml-families/src/configureAsStructureViewItem.js';
 
 const INSERT_TOPICREF_OPERATION_NAMES = [
 	'contextual-insert-topicref--from-template',
