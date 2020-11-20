dbc8caff063be2bdf5fcebf52fbe61e50873ddfb
Alberto Silvestre Montes Linares <as.montes@bi4group.com>
2020-11-20 16:48:53 +0100

    DEV-7921 Image replace operation remains enabled when document is out of sync

--- packages/dita-example-sx-integration/src/install.js
+++ packages/dita-example-sx-integration/src/install.js
@@ -1,6 +1,20 @@
 import addTransform from 'fontoxml-operations/src/addTransform.js';
 
 export default function install() {
+	addTransform(
+		'removeImageValue',
+		function (stepData) {
+			// After checking the state, we go to the next operation to select the reference from image url
+			return stepData;
+		},
+		function removeImageValue(stepData) {
+			// When we get the operation state, we don't still want to select the image, we
+			// only want to check if document is editable
+			stepData.selectedImageId = null;
+			return stepData;
+		}
+	);
+
 	addTransform('setReferenceForUrl', function setReferenceForUrl(stepData) {
 		stepData.reference = stepData.url;
 		return stepData;
