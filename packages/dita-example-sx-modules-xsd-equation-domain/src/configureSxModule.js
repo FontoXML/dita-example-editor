0ace69df44931e95c2c8dcc1e559cab09885b2ae
Alberto Silvestre Montes Linares <alberto.montes@liones.nl>
2021-02-05 14:49:02 +0100

    DEV-7076 MathML equations are inserted at different positions (context menu vs element menu)

--- packages/dita-example-sx-modules-xsd-equation-domain/src/configureSxModule.js
+++ packages/dita-example-sx-modules-xsd-equation-domain/src/configureSxModule.js
@@ -36,11 +36,7 @@ export default function configureSxModule(sxModule) {
 		contextualOperations: [
 			{ name: ':equation-figure-insert-title' },
 			{ name: ':equation-figure-insert-desc' },
-			{ name: ':equation-figure-append-mathml', hideIn: ['context-menu'] },
-			{
-				name: ':equation-figure-insert-mathml',
-				hideIn: ['element-menu', 'breadcrumbs-menu']
-			},
+			{ name: ':equation-figure-append-mathml', hideIn: ['breadcrumbs-menu'] },
 			{ name: ':contextual-delete-equation-figure' }
 		],
 		titleQuery: './title',
