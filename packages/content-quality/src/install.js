7f3563c030fa3b30005e4061144ff1a07da4fc46
Thomas Brekelmans <thomas.brekelmans@liones.nl>
2021-03-02 11:03:23 +0100

    DEV-9922 DEV-9927 DEV-10083 Hunspell integration

--- packages/content-quality/src/install.js
+++ packages/content-quality/src/install.js
@@ -3,6 +3,19 @@ import t from 'fontoxml-localization/src/t.js';
 import contentQualityManager from 'fontoxml-content-quality/src/contentQualityManager.js';
 
 export default function install() {
+	// TODO: cover this category with a Cypress test
+	contentQualityManager.registerAnnotationTypesCategory(
+		{
+			id: 'hunspell-spelling',
+			label: t('Spelling (hunspell)'),
+			description: t(
+				'See suggestions for spelling mistakes from the hunspell powered checker.'
+			)
+		},
+		[{ namespace: 'urn:fontoxml:content-quality:spelling:1.0.0', name: 'spelling-error' }]
+	);
+
+	// TODO: merge these together and update Cypress tests
 	contentQualityManager.registerAnnotationTypesCategory(
 		{
 			id: 'spelling',
