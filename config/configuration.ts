0a251402e24624544b80e7c2354e0e9e3206becf
Erik van der Valk <erik.van.der.valk@liones.nl>
2022-12-23 14:45:49 +0100

    Remove deprecated configuration option.

--- config/configuration.ts
+++ config/configuration.ts
@@ -4,7 +4,6 @@ import xq from 'fontoxml-selectors/src/xq';
 configurationManager.set('paragraph-node-name-for-pasting', 'p');
 
 configurationManager.set('conrefs-use-permanent-references', false);
-configurationManager.set('map-manager-use-permanent-references', false);
 
 configurationManager.set('preferred-locales', ['en']);
 
